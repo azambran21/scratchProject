4a99d794d9508ed6e5e4d405db2edee6794206e4
swierkopa <swierkopa@gmail.com>
2022-04-23 17:36:40 -0400

    api call is working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
+const foodController = require('../getFoodMiddleware');
+// Api Call/openfoodfacts-react-native/API/Api.js
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,4 +19,7 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+app.get('/', foodController.getProduct, (req, res) => {
+  return res.status(200).json(res.locals.searchResults)
+})
 // module.exports = app;
